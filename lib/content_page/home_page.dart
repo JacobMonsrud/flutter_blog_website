b4a4128de2991264561a682af7638673a3dd51c4
JacobMonsrud <jacobkm@live.dk>
2021-10-22 10:21:07 +0200

    Firebase debugging

--- lib/content_page/home_page.dart
+++ lib/content_page/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:amanda_stensgaard/content_page/home_page_desktop.dart';
 import 'package:amanda_stensgaard/content_page/home_page_mobile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
@@ -14,21 +16,27 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  void getImages() async {
+    // First get from firestore. Then if no URL, get URL from storage.
+    CollectionReference collectionRef = FirebaseFirestore.instance.collection('articles');
+    QuerySnapshot querySnapshot = await collectionRef.get();
+    for (var doc in querySnapshot.docs) {
+      print(doc.data().toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    //getImages();
     return Scaffold(
       backgroundColor: Color.fromRGBO(242, 238, 228, 1),
       body: LayoutBuilder(
         builder: (context, constraints){
           if (!isMobile()) {
             return DesktopHomePage();
-            //print(Theme.of(context).platform.toString() == "TargetPlatform.android");
-            //print(MediaQuery.of(context).size.width);
           }
           else {
             return MobileHomePage();
-            //print(MediaQuery.of(context).size.width);
-            //print(Theme.of(context).platform.toString() == "TargetPlatform.android");
           }
         },
       ),
